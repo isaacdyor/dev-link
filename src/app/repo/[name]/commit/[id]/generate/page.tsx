2b9c8a5d4e86a51f26838107c8e86dd61038423b
Isaac Dyor <isaac@dyor.com>
2023-10-14 11:39:35 -0700

    providing github data

--- src/app/repo/[name]/commit/[id]/generate/page.tsx
+++ src/app/repo/[name]/commit/[id]/generate/page.tsx
@@ -3,10 +3,9 @@
 import { useUser } from "@clerk/nextjs";
 import { NextPage } from "next";
 import { Octokit } from "octokit";
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useChat } from "@/lib/useChat";
 import { File } from "@/types/types";
-import { useChat } from "ai/react";
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
@@ -17,15 +16,12 @@ const GeneratePage: NextPage<{ params: { name: string; id: string } }> = ({
 }) => {
   const { user, isLoaded } = useUser();
 
-  const [result, setResult] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [completed, setCompleted] = useState(false);
+  const [requestMade, setRequestMade] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
 
   const { name: repoName, id: commitId } = params;
 
-  const getData = async (): Promise<File[]> => {
+  const getData = async () => {
     try {
       const response = await octokit.request(
         "GET /repos/{owner}/{repo}/commits/{ref}",
@@ -62,15 +58,14 @@ const GeneratePage: NextPage<{ params: { name: string; id: string } }> = ({
 
         updatedFiles.push(fileObject);
       }
-
-      return updatedFiles;
+      setFiles(updatedFiles);
     } catch (error) {
       console.error("Error fetching data from GitHub API:", error);
       throw error; // Throw an error to indicate the failure.
     }
   };
 
-  const generatePrompt = (files: File[]) => {
+  const getGithubDataString = (fileString: File[]): string => {
     const fileStrings = files.map((file) => {
       // Customize the structure of each string as needed
       return `Filename: ${file.filename}\nPatch:\n ${file.patch}\n\nContent:\n ${file.content}\n`;
@@ -79,94 +74,44 @@ const GeneratePage: NextPage<{ params: { name: string; id: string } }> = ({
     // Join the file strings together into one large string
     const combinedString = fileStrings.join("\n\n");
 
-    const prompt =
-      "I am providing you with a list of files changed in a github commit. The file name, patch, and content of the file is included. Can you please create a brief blog article outlining the changes made, what they do and the decision process for why these changes were made? The first piece of content of your response should be the title in the format title: {title goes here} \n\n" +
-      combinedString;
-
-    return prompt;
+    return combinedString;
   };
 
-  async function getAiReponse(prompt: string) {
-    setIsLoading(true);
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: prompt }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
-      setResult(data.result);
-      return response;
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
+  useEffect(() => {
+    if (isLoaded) {
+      getData();
     }
-  }
+  }, [isLoaded]);
 
   const { messages, input, handleInputChange, handleSubmit } = useChat({
+    githubData: getGithubDataString(files),
     api: "/api/chat",
   });
-
-  const generateContent = async (e: React.FormEvent<HTMLFormElement>) => {
-    const data: File[] = await getData();
-    const prompt = generatePrompt(data);
-
-    // const response = await getAiReponse("Say hello world three times");
-    handleSubmit(e);
-  };
-
-  // useEffect(() => {
-  //   if (isLoaded) {
-  //     generateContent();
-  //   }
-  // }, [isLoaded]);
 
   if (!user) return null;
 
   return (
-    <div className="pl-10 pt-10">
-      <input
-        placeholder="title"
-        value={title}
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-        required
-        className="bg-background border-border border"
-      />
-      <textarea
-        placeholder="content"
-        value={content}
-        onChange={(e) => {
-          setContent(e.target.value);
-        }}
-        className="bg-background border-border border"
-        required
-      />
-      <input
-        value={input}
-        onChange={handleInputChange}
-        placeholder="Chat with me please"
-      />
-      {/* <button onClick={(e) => generateContent(e)} className="bg-primary rounded p-2">
-        generate content
-      </button> */}
-
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message.content}</li>
-        ))}
-      </ul>
+    <div>
+      {!requestMade && (
+        <form
+          onSubmit={(e) => {
+            setRequestMade(true);
+            handleSubmit(e);
+          }}
+        >
+          <label>
+            Any other information you would like to add.
+            <input
+              value={input}
+              onChange={handleInputChange}
+              placeholder="extra information"
+              className="bg-secondary"
+            />
+          </label>
+          <button type="submit">Send</button>
+        </form>
+      )}
+      <p>{messages[1]?.content}</p>
     </div>
   );
 };
--- src/lib/getPrompt.ts
+++ src/lib/getPrompt.ts
@@ -1,3 +1,3 @@
 export const getPrompt = (githubData: any, input: string) => {
-  return `${githubData} + ${input}`;
+  return `${input} : ${githubData}`;
 };
